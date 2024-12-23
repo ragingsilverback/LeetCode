5ffbe3da7198645bd936d78e3f1b18ea5b3bc1ca
Abhisar Kushwah <39746445+ragingsilverback@users.noreply.github.com>
2024-12-23 17:12:56 +0530

    Time: 0 ms (100%), Space: 18.1 MB (8.11%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,21 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        def create_adjlist(rooms):
-            adjlist = {i:[] for i in range(len(rooms))}
-            for i in range(len(rooms)):
-                adjlist[i] = [key for key in rooms[i]]
-            return adjlist
-
         visited = set()
-        adjlist = create_adjlist(rooms)
 
-        def dfs(start,visited,adjlist):
+        def dfs(start,visited):
             visited.add(start)
-            for neighbour in adjlist[start]:
-                if neighbour not in visited:
-                    dfs(neighbour,visited,adjlist)
+            for keys in rooms[start]:
+                if keys not in visited:
+                    dfs(keys,visited)
 
-        dfs(0,visited,adjlist)
+        dfs(0,visited)
 
         return len(rooms)==len(visited)
 
